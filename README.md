0ef425c016ea7e5948c22854ae540a3a8cd6351b
nadanime <git@nadanime.com>
2025-07-04 14:10:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SIMPRAK
+
+### Fitur:
+- Login
+![Login](/readme-images/preview-login.png)
+
+- Register
+![Register](/readme-images/preview-register.png)
+
+- Dashboard
+![Dashboard](/readme-images/preview-dashboard.png)
---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Pengaturan Database
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'nada'); 
+define('DB_PASSWORD', $_ENV['DB_PASS']); 
+define('DB_NAME', 'simprak');
+
+// Membuat koneksi ke database
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Koneksi ke database gagal: " . $conn->connect_error);
+}
+?>
